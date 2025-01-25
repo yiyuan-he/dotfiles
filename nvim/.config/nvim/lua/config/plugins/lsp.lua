70c5edbb0bb2d00faa5ad93cba8461a36e0f57d9
yiyuanh <yiyuanh@proton.me>
2025-01-25 11:19:07 -0800

    stop lsp autostart

--- nvim/.config/nvim/lua/config/plugins/lsp.lua
+++ nvim/.config/nvim/lua/config/plugins/lsp.lua
@@ -15,30 +15,15 @@ return {
       },
     },
     config = function()
-      local diagnostics_enabled = false
-
-      local function toggle_diagnostics()
-        diagnostics_enabled = not diagnostics_enabled
-        if diagnostics_enabled then
-          vim.diagnostic.enable(true)
-          print("Diagnostics: Enabled")
-        else
-          vim.diagnostic.enable(false)
-          print("Diagnostics: Disabled")
-        end
-      end
-
-      -- Disable diagnostics at startup
-      vim.diagnostic.enable(false)
-
-      vim.keymap.set('n', '<C-`>', toggle_diagnostics, { desc = "Toggle Diagnostics" } )
-
       require('lspconfig').lua_ls.setup {
+        autostart = false,
         diagnostics = {
           disable = { 'missing-fields' }
         }
       }
-      require('lspconfig').jdtls.setup {}
+      require('lspconfig').jdtls.setup {
+        autostart = false,
+      }
     end,
   }
 }
