4167e1caf5772bdcec5039420f7e689bc51bc096
yiyuanh <yiyuanh@amazon.com>
2025-02-21 10:12:58 -0800

    update colorscheme to catppuccin latte

--- nvim/.config/nvim/lua/config/plugins/everforest.lua
+++ nvim/.config/nvim/lua/config/plugins/everforest.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
-    vim.g.everforest_enable_italic = true
-    vim.cmd.colorscheme('everforest')
+    -- vim.g.everforest_enable_italic = true
+    -- vim.cmd.colorscheme('everforest')
   end
 }
