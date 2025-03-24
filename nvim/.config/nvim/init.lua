0fb2f563aa3d2a66a99c484e1d6a329ed06a0c4f
yiyuanh <yiyuanh@amazon.com>
2025-03-24 16:15:08 -0700

    feat: set up harpoon

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,6 +13,8 @@ require("paq")({
   "echasnovski/mini.nvim",
   "folke/which-key.nvim",
   "itchyny/lightline.vim",
+  "nvim-lua/plenary.nvim",
+  { "ThePrimeagen/harpoon", branch = "harpoon2", requires = { {"nvim-lua/plenary.nvim" } } },
 })
 
 -- Set colorscheme
@@ -200,3 +202,22 @@ require("mini.pairs").setup {
     ['`'] = false,
   },
 }
+
+-- Set up harpoon
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
