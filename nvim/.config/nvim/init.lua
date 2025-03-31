33a71962d7c5d9b7cc261ed5e5bcc610d82f1094
yiyuanh <yiyuanh@amazon.com>
2025-03-31 13:17:12 -0700

    feat: change colorscheme to kanagawa dragon and add zen mode to nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,9 +18,36 @@ require("paq")({
   { "catppuccin/nvim", as = "catppuccin" },
   "folke/tokyonight.nvim",
   "navarasu/onedark.nvim",
+  "folke/zen-mode.nvim",
+  "rebelot/kanagawa.nvim",
 })
 
-vim.cmd.colorscheme "onedark"
+require('kanagawa').setup({
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = true,            -- enable undercurls
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true},
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = false,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "wave",              -- Load "wave" theme
+    background = {               -- map the value of 'background' option to a theme
+        dark = "dragon",           -- try "dragon" !
+        light = "lotus"
+    },
+})
+
+vim.cmd("colorscheme kanagawa")
 
 -- Set up LSP
 local lspconfig = require("lspconfig")
