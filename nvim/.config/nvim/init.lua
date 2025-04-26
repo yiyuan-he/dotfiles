cd8219e4eca4adb974344c49cad26e377a27cf75
yiyuanh <yiyuanh@proton.me>
2025-04-26 10:45:34 -0700

    tweak open diagnostic keybind

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -65,7 +65,7 @@ for _, lsp in pairs(lsps) do
   lspconfig[lsp].setup(setup)
 end
 
-vim.keymap.set("n", "<space>ld", "<cmd>lua vim.diagnostic.open_float()<CR>")
+vim.keymap.set("n", "<space>d", "<cmd>lua vim.diagnostic.open_float()<CR>")
 
 -- Configure LazyDev
 vim.api.nvim_create_autocmd("FileType", {
