d0fd285960dce11681bf52d7105d1ecbc1ef8ff7
yiyuanh <yiyuanh@proton.me>
2025-07-10 14:36:52 -0700

    update fish config colors

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,53 +18,53 @@ set -gx VISUAL nvim
 # Sun Mage Fish Colors - Focus-enhancing light theme
 
 # Color variables for semantic use
-set -g alt 626262      # neutral gray
-set -g constant 7a9ec2 # peaceful blue
-set -g comment 626262  # neutral gray for comments
-set -g fg 4a4a4a       # soft charcoal
-set -g func 8da474     # fresh sage for functions
-set -g keyword 6d8b42  # sage green for keywords
-set -g number 9e83a8   # lavender for numbers
-set -g operator ca7073 # soft coral for operators
-set -g property 5485b6 # calm sky blue for properties
-set -g str c09553      # warm amber for strings
-set -g type 8da474     # fresh sage for types
+set -g alt 4a4a4a      # darker gray
+set -g constant 5a7ea2 # darker blue
+set -g comment 4a4a4a  # darker gray for comments
+set -g fg 1a1a1a       # very dark charcoal
+set -g func 4d6b22     # darker sage for functions
+set -g keyword 2a4d0e  # much darker sage green for keywords
+set -g number 7e6388   # darker lavender for numbers
+set -g operator aa5053 # darker coral for operators
+set -g property 345596 # darker sky blue for properties
+set -g str a07533      # darker amber for strings
+set -g type 4d6b22     # darker sage for types
 
 # Syntax Highlighting Colors
-set -g fish_color_normal 4a4a4a
-set -g fish_color_command 8da474
-set -g fish_color_keyword 6d8b42
-set -g fish_color_quote c09553
-set -g fish_color_redirection 7a9ec2
-set -g fish_color_end ca7073
-set -g fish_color_error b85450
-set -g fish_color_param 4a4a4a
-set -g fish_color_valid_path 8da474
-set -g fish_color_option 85678f
-set -g fish_color_comment 626262
+set -g fish_color_normal 1a1a1a
+set -g fish_color_command 65476f
+set -g fish_color_keyword 2a4d0e
+set -g fish_color_quote a07533
+set -g fish_color_redirection aa5053
+set -g fish_color_end aa5053
+set -g fish_color_error 985030
+set -g fish_color_param 1a1a1a
+set -g fish_color_valid_path a07533
+set -g fish_color_option 65476f
+set -g fish_color_comment 4a4a4a
 set -g fish_color_selection --background=ede7dc
-set -g fish_color_operator ca7073
-set -g fish_color_escape 6d8b42
-set -g fish_color_autosuggestion 626262
-set -g fish_color_cwd c09553
-set -g fish_color_hostname 9e83a8
-set -g fish_color_status b85450
-set -g fish_color_cancel 9e83a8
+set -g fish_color_operator aa5053
+set -g fish_color_escape 2a4d0e
+set -g fish_color_autosuggestion 4a4a4a
+set -g fish_color_cwd a07533
+set -g fish_color_hostname 7e6388
+set -g fish_color_status 985030
+set -g fish_color_cancel 7e6388
 set -g fish_color_search_match --background=ede7dc
 
 # Completion Pager Colors
-set -g fish_pager_color_progress 85678f
-set -g fish_pager_color_prefix 8da474
-set -g fish_pager_color_completion 4a4a4a
-set -g fish_pager_color_description 626262
-set -g fish_pager_color_selected_prefix 6d8b42
-set -g fish_pager_color_selected_completion 6d8b42
+set -g fish_pager_color_progress 65476f
+set -g fish_pager_color_prefix 65476f
+set -g fish_pager_color_completion 1a1a1a
+set -g fish_pager_color_description 4a4a4a
+set -g fish_pager_color_selected_prefix 65476f
+set -g fish_pager_color_selected_completion 65476f
 set -g fish_pager_color_selected_background --background=ede7dc
 
 # FZF Light Theme Colors - Matching Sun Mage palette
 export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS
---color=fg:#4a4a4a,bg:#faf8f3,hl:#5485b6,gutter:#faf8f3
---color=fg+:#4a4a4a,bg+:#ede7dc,hl+:#6d8b42
+--color=fg:#2a2a2a,bg:#faf8f3,hl:#5485b6,gutter:#faf8f3
+--color=fg+:#1a1a1a,bg+:#ede7dc,hl+:#6d8b42
 --color=info:#7a9ec2,prompt:#5485b6,pointer:#9e83a8
 --color=marker:#5485b6,spinner:#5485b6,header:#5485b6"
 
