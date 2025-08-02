e972ba3c945b05f2dc597c3683bcc3d5a58be610
yiyuanh <yiyuanh@proton.me>
2025-08-02 07:54:09 -0700

    try out lazy vim config for mini pairs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -207,29 +207,16 @@ require('oil').setup()
 vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 
 require("mini.pairs").setup {
-  -- In which modes mappings from this `config` should be created
-  modes = { insert = true, command = false, terminal = false },
-
-  -- Global mappings. Each right hand side should be a pair information, a
-  -- table with at least these fields (see more in |MiniPairs.map|):
-  -- - <action> - one of 'open', 'close', 'closeopen'.
-  -- - <pair> - two character string for pair to be used.
-  -- By default pair is not inserted after `\`, quotes are not recognized by
-  -- `<CR>`, `'` does not insert pair after a letter.
-  -- Only parts of tables can be tweaked (others will use these defaults).
-  mappings = {
-    ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
-    ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
-    ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
-
-    [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
-    [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
-    ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
-
-    ['"'] = false,
-    ["'"] = false,
-    ['`'] = false,
-  },
+  modes = { insert = true, command = true, terminal = false },
+  -- skip autopair when next character is one of these
+  skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+  -- skip autopair when the cursor is inside these treesitter nodes
+  skip_ts = { "string" },
+  -- skip autopair when next character is closing pair
+  -- and there are more closing pairs than opening pairs
+  skip_unbalanced = true,
+  -- better deal with markdown code blocks
+  markdown = true,
 }
 
 -- Set up harpoon
