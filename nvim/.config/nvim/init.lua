33ab175e885fffb1f7c2c23239f0f5266cc22ec3
yiyuanh <yiyuanh@amazon.com>
2025-09-23 15:44:59 -0700

    remove markdown viewer in nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -272,17 +272,6 @@ require("lazy").setup({
     opts = {},
   },
 
-  -- Markdown View
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-  },
-
   -- Mini.nvim modules
   {
     "echasnovski/mini.nvim",
