d7f9e0bf1a7d44ac852359e5ab6ec775707f5ff6
yiyuanh <yiyuanh@amazon.com>
2025-09-23 09:48:00 -0700

    change colorscheme to tokyonight

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -88,56 +88,16 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   -- Colorschemes
   {
-    "vague2k/vague.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other plugins
-    config = function()
-      -- NOTE: you do not need to call setup if you don't want to.
-      require("vague").setup({
-        -- optional configuration here
-      })
-      vim.cmd("colorscheme vague")
-    end
-  },
-  {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+      vim.cmd("colorscheme tokyonight-night")
+    end
   },
   { "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-  },
-  {
-    "cdmill/neomodern.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "gbprod/nord.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    'everviolet/nvim', name = 'evergarden',
-    priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-    opts = {
-      theme = {
-        variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
-        accent = 'green',
-      },
-      editor = {
-        transparent_background = false,
-        sign = { color = 'none' },
-        float = {
-          color = 'mantle',
-          solid_border = false,
-        },
-        completion = {
-          color = 'surface0',
-        },
-      },
-    },
   },
 
   -- LSP & Development
