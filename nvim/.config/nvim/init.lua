41257459fdf6e32af59e460c1c6c830834e30845
Michael He <yiyuanh@proton.me>
2025-10-07 22:05:51 +0000

    add nvim snacks

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -260,6 +260,34 @@ require("lazy").setup({
     },
   },
 
+  -- Snacks
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      picker = { enabled = true },
+      quickfile = { enabled = true },
+      image = { enabled = true },
+      gitbrowse = { enabled = true },
+    },
+    keys = {
+      -- LSP
+      { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+      { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+      { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+      { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+      { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+      { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+      { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+      { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse", mode = { "n", "v" } },
+    }
+  },
+
   -- Dependencies
   { "nvim-lua/plenary.nvim", lazy = true },
 })
