3776e6a328220a5d81de27d8564057d1ee51f0ee
Michael He <yiyuanh@proton.me>
2025-10-07 18:38:23 -0700

    fix treesitter branch tracking

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -137,28 +137,16 @@ require("lazy").setup({
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    branch = "main",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", 
-          "javascript", "html", "go", "rust", "python", "cpp", 
-          "ocaml", "markdown", "markdown_inline", "ninja", "rst",
-        },
-        auto_install = false,
+      require("nvim-treesitter").setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        auto_install = true,
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true, disable = {"ocaml"} },
-        ---@diagnostic disable-next-line: unused-local
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-        additional_vim_regex_highlighting = false,
       })
     end,
   },
