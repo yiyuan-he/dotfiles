6785326b8012932cfdc0eaf249394b45bc82ca30
yiyuanh <yiyuanh@proton.me>
2026-02-08 09:45:13 -0800

    change theme switcher script to use tokyo night

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -263,13 +263,14 @@ require("lazy").setup({
   { "cocopon/iceberg.vim" },
   { "sainnhe/edge", lazy = false, priority = 1000 },
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "navarasu/onedark.nvim", priority = 1000 }
+  { "navarasu/onedark.nvim", priority = 1000 },
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {}, }
 })
 
 -- ============================================================================
 -- Colorscheme
 -- ============================================================================
-vim.cmd.colorscheme("xcode-light")
+vim.cmd.colorscheme("tokyonight-moon")
 
 -- ============================================================================
 -- Keymaps
